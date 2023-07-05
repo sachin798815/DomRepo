3455cc014c2d3f575e49690b1b5ce7be7faa4701
sachin798815 <sachin798815@gmail.com>
2023-07-05 23:41:26 +0530

    Task5

---
+++ domT5.js
@@ -0,0 +1,6 @@
+let classLi = document.getElementsByClassName('list-group-item');
+let tagLi = document.getElementsByTagName('li');
+// console.log(classLi);
+// console.log(tagLi);
+tagLi[4].style.color = 'red';
+console.log(tagLi[4]);
